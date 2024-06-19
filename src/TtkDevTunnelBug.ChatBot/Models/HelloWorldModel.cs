2bf36523d877c9ee240a1baef1a04a34abd35004
Anth Lubic <alubic@webstaurantstore.com>
2024-06-19 07:53:35 -0400

    scaffold projects to reproduce bug

---
+++ src/TtkDevTunnelBug.ChatBot/Models/HelloWorldModel.cs
@@ -0,0 +1,9 @@
+﻿namespace TtkDevTunnelBug.ChatBot.Models
+{
+    public class HelloWorldModel
+    {
+        public string Title { get; set; }
+
+        public string Body { get; set; }
+    }
+}
